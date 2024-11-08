49af8b4631e89fb91346a47fd90ac6a156b332bc
jakub.koza <kubulax25@gmail.com>
2024-11-08 09:53:58 +0100

    Rozpoczęto prace nad aplikacją

--- Program.cs
+++ Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(StringHelper.countVovels("aab"));
+            Console.WriteLine(StringHelper.removeRepetitions("aaabba"));
+        }
+    }
+
+    public static class StringHelper
+    {
+        public static int countVovels(string text)
+        {
+            int numberOfVovels = 0;
+
+            if(text == null || string.IsNullOrEmpty(text))
+            {
+                return numberOfVovels;
+            }
+            string vovels = "aąeęiouóyAĄEĘIOUÓY";
+
+            foreach(char character in text)
+            {
+                foreach(char vovel in vovels) 
+                {
+                    if(character == vovel)
+                    {
+                        numberOfVovels++;
+                    }
+                }
+            }
+
+            return numberOfVovels;
+        }
+
+        public static string removeRepetitions(string text)
+        {
+            string stringWithoutRepetitions = text[0].ToString();
+
+            if (text == null || string.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+
+            char currentCharacter = text[0];
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(i < text.Length - 1 && currentCharacter != text[i + 1])
+                {
+                    stringWithoutRepetitions += text[i + 1];
+                }
+
+                currentCharacter = text[i];
+            }
+
+            return stringWithoutRepetitions;
         }
     }
 }
